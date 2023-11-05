e6e9552f7b886a82d5abb74a6f0a7dd8d48afb2d
amarthyalekshmi <145977423+amarthyalekshmi@users.noreply.github.com>
2023-11-04 23:46:59 -0700

    Update Untitled11.py

--- Untitled11.py
+++ Untitled11.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 def gcd(a,b):
     if(b==0):
         print(a)
@@ -12,16 +6,3 @@ def gcd(a,b):
 a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
 gcd(a,b)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
