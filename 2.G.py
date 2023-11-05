c3e24a76596d509a6c382273f8275dae067969b5
amarthyalekshmi <145977423+amarthyalekshmi@users.noreply.github.com>
2023-11-04 23:56:05 -0700

    Update and rename Untitled3.py to 2.G.py

--- 2.G.py
+++ 2.G.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 list1=[]
 for i in range(0,5):
     n=int(input("enter the integer:"))
@@ -13,22 +7,3 @@ for i in list1:
     if(i%2==0):
         list1.remove(i)
 print("after removing even number:",list1)        
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
