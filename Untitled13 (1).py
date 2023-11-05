9d2a0c7000cbe5bbd6a5bfbc83a9b0895e2fe919
amarthyalekshmi <145977423+amarthyalekshmi@users.noreply.github.com>
2023-11-05 00:00:20 -0700

    Update Untitled13 (1).py

--- Untitled13 (1).py
+++ Untitled13 (1).py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 list1=[]
 square=[]
 for i in range(0,5):
@@ -12,6 +6,3 @@ for i in range(0,5):
 print(list1)
 square=[i*i for i in list1]
 print("the square of the given numbers:",square)
-
-
-# 
